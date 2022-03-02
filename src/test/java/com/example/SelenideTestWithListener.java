100d0c57a2c5d1d43d9f1bdde3fccfcdff184727
Nikita Sukhodoev <dogmach94@yandex.ru>
2022-03-02 13:38:15 +0400

    1

--- src/test/java/com/example/SelenideTestWithListener.java
+++ src/test/java/com/example/SelenideTestWithListener.java
@@ -3,27 +3,33 @@ package com.example;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selectors.withText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
-public class SelenideTest {
+public class SelenideTestWithListener {
+
+    @AfterEach
+    void afterEach(){
+        closeWebDriver();
+    }
+
     @Test
-    void testIssueSearch(){
+    void testIssueSearch() {
         SelenideLogger.addListener("allure", new AllureSelenide());
 
         open("https://github.com/");
 
         $(".header-search-input").click();
-        $(".header-search-input").sendKeys("eroshenkoam/allure-example");
+        $(".header-search-input").sendKeys("Dogmach/qa_guru_allure_lecture_1");
         $(".header-search-input").submit();
 
-        $(By.linkText("eroshenkoam/allure-example")).click();
+        $(By.linkText("Dogmach/qa_guru_allure_lecture_1")).click();
         $(By.partialLinkText("Issues")).click();
-        $(withText("#68")).should(Condition.exist);
+        $(withText("Issue test for homework")).should(Condition.exist);
 
     }
 }
